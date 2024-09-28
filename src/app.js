715c6ed3380eac37560443783effd2fa383fe603
Tanmay <tanmaytambat01@gmail.com>
2024-09-28 23:48:51 +0530

    fix vercel config

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use(
 );
 app.use(express.json({ limit: "16kb" })); //!Define the max incoming json size
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); //!Define the max size of data from url
-app.use(express.static(join(__dirname, "../public")));
+app.use(express.static(join(__dirname, "..", "/public")));
 app.use(cookieParser());
 
 //*Routes
